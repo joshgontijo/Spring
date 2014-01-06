d88e59216caabebf1e09084fbec706df5ec4f3d1
Josue <josue@gpnxgroup.com>
2014-01-06 18:47:35 -0200

    updated liquibase-ejb tests

--- Liquibase-EJB/src/test/java/com/josue/liqui/liquibase/ejb/PersonTest.java
+++ Liquibase-EJB/src/test/java/com/josue/liqui/liquibase/ejb/PersonTest.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.josue.liqui.liquibase.ejb;
 
 import java.util.UUID;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import org.junit.After;
@@ -19,53 +19,109 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Josue
  */
 public class PersonTest {
+
+    EntityManagerFactory emf;
+    EntityManager em;
+
+    private final static Logger logger = LoggerFactory.getLogger(PersonTest.class);
     
     public PersonTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
+        emf = Persistence.createEntityManagerFactory("TEST-MYSQL-PU");
+        em = emf.createEntityManager();
     }
-    
+
     @After
     public void tearDown() {
+        em.close();
+        emf.close();
     }
 
-
     @Test
-    public void testX() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TEST-PU");
-        EntityManager em = emf.createEntityManager();
-        
+    public void testSimple() {
+
         String personUuid = UUID.randomUUID().toString();
-        
+
         Person person = new Person();
         person.setUuid(personUuid);
         person.setPassword("psw");
         person.setUsername("username");
-        
+
         em.persist(person);
-        
         Person person2 = em.find(Person.class, personUuid);
-        
+
         assertNotNull(person2);
         assertEquals(person, person2);
     }
 
-    
-    
+    @Test
+    public void testTransaction() {
+
+        EntityTransaction transaction = em.getTransaction();
+
+        String personUuid = UUID.randomUUID().toString();
+
+        Person person = new Person();
+        person.setUuid(personUuid);
+        person.setPassword("psw");
+        person.setUsername("username");
+
+        transaction.begin();
+        em.persist(person);
+        transaction.commit();
+
+        Person person2 = em.find(Person.class, personUuid);
+
+        assertNotNull(person2);
+        assertEquals(person, person2);
+    }
+
+    @Test
+    public void testTransactionFail() {
+
+        EntityTransaction transaction = em.getTransaction();
+
+        String personUuid = UUID.randomUUID().toString();
+
+        Person person = new Person();
+        person.setUuid(personUuid);
+        person.setPassword(null);
+        person.setUsername("username2");
+
+        try {
+            transaction.begin();
+            em.persist(person);
+            transaction.commit();
+            logger.info("Success !");
+        } catch (Exception e) {
+            logger.info("EXCEPTION **** "+ e.getMessage());
+            System.err.println(e.getMessage());
+            transaction.rollback();
+//            logger.info("EXCEPTION **** "+ e.getMessage());
+        }
+//        Person person2 = em.find(Person.class, personUuid);
+//
+//        assertNotNull(person2);
+//        assertEquals(person, person2);
+    }
+
 }
